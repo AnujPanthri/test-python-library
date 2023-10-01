f159d63a706166ffeb38a9bdf9bd3b24dfabbe8d
Ankur Panthri <panthrianuj@gmail.com>
2023-10-01 12:11:07 +0530

    changed version to 0.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md","r",encoding="utf-8") as f:
 
 setuptools.setup(
     name = "test-python-library",
-    version ='0.0.1',
+    version ='0.0.2',
     author = "Anuj Panthri",
     author_email="panthrianuj@gmail.com",
     description="just a sample library",
